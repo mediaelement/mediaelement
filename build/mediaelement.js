651fcd15e9286cba6e2154131a4f2866b779277f
gavinlynch <gavin.b.lynch@gmail.com>
2010-11-23 20:45:03 -0600

    media type tweaks

--- build/mediaelement.js
+++ build/mediaelement.js
@@ -24,10 +24,10 @@ mejs.meIndex = 0;
 // media types accepted by plugins
 mejs.plugins = {
 	silverlight: [
-		{version: [3,0], types: ['video/mp4','video/m4v','video/mov','video/wmv','audio/wma','audio/mp4','audio/m4a','audio/mp3']}
+		{version: [3,0], types: ['video/mp4','video/m4v','video/mov','video/wmv','audio/wma','audio/m4a','audio/mp3','audio/wav']}
 	],
 	flash: [
-		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','audio/flv','audio/mp3','audio/m4a','audio/mp3']}		
+		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','audio/flv','audio/mp3','audio/m4a']}		
 		//,{version: [11,0], types: ['video/webm'} // for future reference
 	]
 };
