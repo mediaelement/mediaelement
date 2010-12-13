4e0dc5a6dc21b808a95f8af36704989ad31e4048
johndyer <johndyer@gmail.com>
2010-12-13 16:22:11 -0600

    - 2.0.0 final

--- build/mediaelement.js
+++ build/mediaelement.js
@@ -1,17 +1,16 @@
 /*!
-* Media Element
+* MediaElement.js
 * HTML5 <video> and <audio> shim and player
 * http://mediaelementjs.com/
 *
-* Creates a JavaScript object that mimics HTML5 media object
+* Creates a JavaScript object that mimics HTML5 MediaElement API
 * for browsers that don't understand HTML5 or can't play the provided codec
-* Can also play MP4 (H.264), Ogg, WebM, FLV, WMV, WMA, ACC, and MP3
+* Can play MP4 (H.264), Ogg, WebM, FLV, WMV, WMA, ACC, and MP3
 *
-* Copyright 2010, John Dyer
+* Copyright 2010, John Dyer (http://johndyer.me)
 * Dual licensed under the MIT or GPL Version 2 licenses.
 *
 */
-
 // Namespace
 var mejs = mejs || {};
 
