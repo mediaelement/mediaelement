e2c88412c83092137601d08e760dd3cd579bf004
gavinlynch <gavin.b.lynch@gmail.com>
2011-01-13 09:35:46 -0600

    - Updated number handling (for the Acrobat detector)

--- src/js/me-plugindetector.js
+++ src/js/me-plugindetector.js
@@ -26,17 +26,17 @@ mejs.PluginDetector = {
 	detectPlugin: function(pluginName, mimeType, activeX, axDetect) {
 		
 		var version = [0,0,0],
-			d,
+			description,
 			i,
 			ax;
 		
 		// Firefox, Webkit, Opera
 		if (typeof(this.nav.plugins) != 'undefined' && typeof this.nav.plugins[pluginName] == 'object') {
-			d = this.nav.plugins[pluginName].description;
-			if (d && !(typeof this.nav.mimeTypes != 'undefined' && this.nav.mimeTypes[mimeType] && !this.nav.mimeTypes[mimeType].enabledPlugin)) {
-				version = d.replace(pluginName, '').replace(/^\s+/,'').replace(/\sr/gi,'.').split('.');
+			description = this.nav.plugins[pluginName].description;
+			if (description && !(typeof this.nav.mimeTypes != 'undefined' && this.nav.mimeTypes[mimeType] && !this.nav.mimeTypes[mimeType].enabledPlugin)) {
+				version = description.replace(pluginName, '').replace(/^\s+/,'').replace(/\sr/gi,'.').split('.');
 				for (i=0; i<version.length; i++) {
-					version[i] = parseInt(version[i], 10);
+					version[i] = parseInt(version[i].match(/\d/), 10);
 				}
 			}
 		// Internet Explorer / ActiveX
@@ -92,10 +92,11 @@ mejs.PluginDetector.addPlugin('silverlight','Silverlight Plug-In','application/x
 /*
 PluginDetector.addPlugin('acrobat','Adobe Acrobat','application/pdf','AcroPDF.PDF', function (ax) {	
 	var version = [],
-		d = ax.GetVersions().split(',')[0].split('=')[1];
+		d = ax.GetVersions().split(',')[0].split('=')[1].split('.');
+	
 	if (d) {
 		version = [parseInt(d[0], 10), parseInt(d[1], 10), parseInt(d[2], 10)];
 	}
 	return version;		
-}
+});
 */
