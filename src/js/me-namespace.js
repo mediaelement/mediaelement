bbdf6034decc58d22ba97b90ecc9c72b9106793a
gavinlynch <gavin.b.lynch@gmail.com>
2011-02-10 15:45:10 -0600

    - added audio/mpeg to flash and silverlight

--- src/js/me-namespace.js
+++ src/js/me-namespace.js
@@ -10,10 +10,10 @@ mejs.meIndex = 0;
 // media types accepted by plugins
 mejs.plugins = {
 	silverlight: [
-		{version: [3,0], types: ['video/mp4','video/m4v','video/mov','video/wmv','audio/wma','audio/m4a','audio/mp3','audio/wav']}
+		{version: [3,0], types: ['video/mp4','video/m4v','video/mov','video/wmv','audio/wma','audio/m4a','audio/mp3','audio/wav','audio/mpeg']}
 	],
 	flash: [
-		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','audio/flv','audio/mp3','audio/m4a']}
+		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','audio/flv','audio/mp3','audio/m4a','audio/mpeg']}
 		//,{version: [11,0], types: ['video/webm']} // for future reference
 	]
 };
