5ed7f273b6b393aaa8a8d01bd4b734095c5bb42f
johndyer <johndyer@gmail.com>
2011-02-17 12:04:22 -0600

    - reverting to 2.0.7 release from pull requests

--- build/mediaelementplayer.js
+++ build/mediaelementplayer.js
@@ -452,26 +452,6 @@
 	}
 })(jQuery);
 (function($) {
-	// STOP BUTTON
-	MediaElementPlayer.prototype.buildstop = function(player, controls, layers, media) {
-		var play = 
-			$('<div class="mejs-button mejs-stop-button mejs-stop">' +
-				'<span></span>' +
-			'</div>')
-			.appendTo(controls)
-			.click(function() {
-				media.pause();
-				media.setCurrentTime(0);				
-				controls.find('.mejs-time-current').width("0px");
-				controls.find('.mejs-time-handle').css("left", "0px");
-				controls.find('.mejs-time-float-current').html( mejs.Utility.secondsToTimeCode(0) );
-				controls.find('.mejs-currenttime').html( mejs.Utility.secondsToTimeCode(0) );
-			});
-			
-			
-	}
-})(jQuery);
-(function($) {
 	// progress/loaded bar
 	MediaElementPlayer.prototype.buildprogress = function(player, controls, layers, media) {
 
