f2cbaa1fd1189d5e63a3aa2a1e0eae9570db1892
johndyer <johndyer@gmail.com>
2011-02-22 10:38:47 -0600

    - fixed names and did some error checking

--- src/js/mep-feature-stop.js
+++ src/js/mep-feature-stop.js
@@ -1,20 +1,23 @@
 (function($) {
 	// STOP BUTTON
 	MediaElementPlayer.prototype.buildstop = function(player, controls, layers, media) {
-		var play = 
+		var stop = 
 			$('<div class="mejs-button mejs-stop-button mejs-stop">' +
 				'<span></span>' +
 			'</div>')
 			.appendTo(controls)
 			.click(function() {
-				media.pause();
-				media.setCurrentTime(0);				
-				controls.find('.mejs-time-current').width("0px");
-				controls.find('.mejs-time-handle').css("left", "0px");
-				controls.find('.mejs-time-float-current').html( mejs.Utility.secondsToTimeCode(0) );
-				controls.find('.mejs-currenttime').html( mejs.Utility.secondsToTimeCode(0) );
+				if (!media.paused) {
+					media.pause();
+				}
+				if (media.currentTime > 0) {
+					media.setCurrentTime(0);	
+					controls.find('.mejs-time-current').width('0px');
+					controls.find('.mejs-time-handle').css('left', '0px');
+					controls.find('.mejs-time-float-current').html( mejs.Utility.secondsToTimeCode(0) );
+					controls.find('.mejs-currenttime').html( mejs.Utility.secondsToTimeCode(0) );					
+					layers.find('.mejs-poster').show();
+				}
 			});
-			
-			
 	}
 })(jQuery);
