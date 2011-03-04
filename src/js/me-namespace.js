62114272827597215129359a00b36ed8c1f400ed
gavinlynch <gavin.b.lynch@gmail.com>
2011-03-03 22:48:58 -0600

    - added flv-x to mimetypes

--- src/js/me-namespace.js
+++ src/js/me-namespace.js
@@ -13,7 +13,7 @@ mejs.plugins = {
 		{version: [3,0], types: ['video/mp4','video/m4v','video/mov','video/wmv','audio/wma','audio/m4a','audio/mp3','audio/wav','audio/mpeg']}
 	],
 	flash: [
-		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','audio/flv','audio/mp3','audio/m4a','audio/mpeg']}
+		{version: [9,0,124], types: ['video/mp4','video/m4v','video/mov','video/flv','video/x-flv','audio/flv','audio/x-flv','audio/mp3','audio/m4a','audio/mpeg']}
 		//,{version: [11,0], types: ['video/webm']} // for future reference
 	]
 };
