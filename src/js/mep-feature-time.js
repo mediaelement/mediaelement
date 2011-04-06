679e20757969aa2de7f754d1b4a82f0165304f74
Sean Hellwig <seanhellwig@gmail.com>
2011-04-05 23:15:02 -0700

    Issue #100 - mejs-time and mejs-duration should support hour:min:sec format. added hh:mm:ss time format

--- src/js/mep-feature-time.js
+++ src/js/mep-feature-time.js
@@ -2,7 +2,7 @@
 	// current and duration 00:00 / 00:00
 	MediaElementPlayer.prototype.buildcurrent = function(player, controls, layers, media) {
 		$('<div class="mejs-time">'+
-				'<span class="mejs-currenttime">00:00</span>'+
+				'<span class="mejs-currenttime">00:00:00</span>'+
 			'</div>')
 			.appendTo(controls);
 
@@ -16,12 +16,12 @@
 	MediaElementPlayer.prototype.buildduration = function(player, controls, layers, media) {
 		if (controls.children().last().find('.mejs-currenttime').length > 0) {
 			$(' <span> | </span> '+
-			   '<span class="mejs-duration">00:00</span>')
+			   '<span class="mejs-duration">00:00:00</span>')
 				.appendTo(controls.find('.mejs-time'));
 		} else {
 
 			$('<div class="mejs-time">'+
-				'<span class="mejs-duration">00:00</span>'+
+				'<span class="mejs-duration">00:00:00</span>'+
 			'</div>')
 			.appendTo(controls);
 		}
