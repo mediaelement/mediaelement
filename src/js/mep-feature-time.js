2bf1e765a2a06dd02f0f23814b623a3c29ed1794
johndyer <johndyer@gmail.com>
2011-05-28 08:42:27 -0500

    - fix error that can happen when duration is not in the controls

--- src/js/mep-feature-time.js
+++ src/js/mep-feature-time.js
@@ -1,12 +1,14 @@
 (function($) {
 	// current and duration 00:00 / 00:00
 	MediaElementPlayer.prototype.buildcurrent = function(player, controls, layers, media) {
+		var t = this;
+		
 		$('<div class="mejs-time">'+
 				'<span class="mejs-currenttime">' + (player.options.alwaysShowHours ? '00:' : '') + '00:00</span>'+
 			'</div>')
 			.appendTo(controls);
 		
-		this.currenttime = this.controls.find('.mejs-currenttime');
+		t.currenttime = t.controls.find('.mejs-currenttime');
 
 		media.addEventListener('timeupdate',function() {
 			player.updateCurrent();
@@ -14,6 +16,8 @@
 	};
 
 	MediaElementPlayer.prototype.buildduration = function(player, controls, layers, media) {
+		var t = this;
+		
 		if (controls.children().last().find('.mejs-currenttime').length > 0) {
 			$(' <span> | </span> '+
 			   '<span class="mejs-duration">' + (player.options.alwaysShowHours ? '00:' : '') + '00:00</span>')
@@ -26,7 +30,7 @@
 			.appendTo(controls);
 		}
 		
-		this.durationD = this.controls.find('.mejs-duration');
+		t.durationD = t.controls.find('.mejs-duration');
 
 		media.addEventListener('timeupdate',function() {
 			player.updateDuration();
@@ -36,13 +40,15 @@
 	MediaElementPlayer.prototype.updateCurrent = function() {
 		var t = this;
 
-		t.currenttime.html(mejs.Utility.secondsToTimeCode(t.media.currentTime | 0, t.options.alwaysShowHours || t.media.duration > 3600 ));
+		if (t.currenttime) {
+			t.currenttime.html(mejs.Utility.secondsToTimeCode(t.media.currentTime | 0, t.options.alwaysShowHours || t.media.duration > 3600 ));
+		}
 	}
 	MediaElementPlayer.prototype.updateDuration = function() {	
 		var t = this;
 		
-		if (t.media.duration) {
-			this.durationD.html(mejs.Utility.secondsToTimeCode(t.media.duration, t.options.alwaysShowHours));
+		if (t.media.duration && t.durationD) {
+			t.durationD.html(mejs.Utility.secondsToTimeCode(t.media.duration, t.options.alwaysShowHours));
 		}		
 	};	
 
