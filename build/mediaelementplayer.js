a037cbefa235efabcf33e46cd96191794e63dfec
johndyer <johndyer@gmail.com>
2011-05-28 08:44:32 -0500

    - 2.1.5 release

--- build/mediaelementplayer.js
+++ build/mediaelementplayer.js
@@ -756,12 +756,14 @@
 (function($) {
 	// current and duration 00:00 / 00:00
 	MediaElementPlayer.prototype.buildcurrent = function(player, controls, layers, media) {
+		var t = this;
+		
 		$('<div class="mejs-time">'+
 				'<span class="mejs-currenttime">' + (player.options.alwaysShowHours ? '00:' : '') + '00:00</span>'+
 			'</div>')
 			.appendTo(controls);
 		
-		this.currenttime = this.controls.find('.mejs-currenttime');
+		t.currenttime = t.controls.find('.mejs-currenttime');
 
 		media.addEventListener('timeupdate',function() {
 			player.updateCurrent();
@@ -769,6 +771,8 @@
 	};
 
 	MediaElementPlayer.prototype.buildduration = function(player, controls, layers, media) {
+		var t = this;
+		
 		if (controls.children().last().find('.mejs-currenttime').length > 0) {
 			$(' <span> | </span> '+
 			   '<span class="mejs-duration">' + (player.options.alwaysShowHours ? '00:' : '') + '00:00</span>')
@@ -781,7 +785,7 @@
 			.appendTo(controls);
 		}
 		
-		this.durationD = this.controls.find('.mejs-duration');
+		t.durationD = t.controls.find('.mejs-duration');
 
 		media.addEventListener('timeupdate',function() {
 			player.updateDuration();
@@ -791,13 +795,15 @@
 	MediaElementPlayer.prototype.updateCurrent = function() {
 		var t = this;
 
-		t.currenttime.html(mejs.Utility.secondsToTimeCode(t.media.currentTime | 0, t.options.alwaysShowHours || t.media.duration > 3600 ));
+		if (t.currenttime) {
+			t.currenttime.html(mejs.Utility.secondsToTimeCode(t.media.currentTime | 0, t.options.alwaysShowHours || t.media.duration > 3600 ));
+		}
 	}
 	MediaElementPlayer.prototype.updateDuration = function() {	
 		var t = this;
 		
-		if (t.media.duration) {
-			this.durationD.html(mejs.Utility.secondsToTimeCode(t.media.duration, t.options.alwaysShowHours));
+		if (t.media.duration && t.durationD) {
+			t.durationD.html(mejs.Utility.secondsToTimeCode(t.media.duration, t.options.alwaysShowHours));
 		}		
 	};	
 
