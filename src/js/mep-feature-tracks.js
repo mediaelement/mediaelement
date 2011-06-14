22dfe63fd02b0b401e1cf7672bf867bc72444eac
johndyer <johndyer@gmail.com>
2011-06-14 10:18:00 -0500

    - changed 'disabled' for jQuery 1.6

--- src/js/mep-feature-tracks.js
+++ src/js/mep-feature-tracks.js
@@ -239,7 +239,7 @@
 
 			t.captionsButton
 				.find('input[value=' + lang + ']')
-					.attr('disabled','')
+					.prop('disabled',false)
 				.siblings('label')
 					.html( mejs.language.codes[lang] || lang );
 
