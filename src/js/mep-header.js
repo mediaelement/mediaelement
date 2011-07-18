56276e1c10bfa0248697d9bab4faa4bb88c1a456
johndyer <johndyer@gmail.com>
2011-07-18 14:56:16 -0500

    - adjusted contact and year info

--- src/js/mep-header.js
+++ src/js/mep-header.js
@@ -5,7 +5,7 @@
  * Creates a controller bar for HTML5 <video> add <audio> tags
  * using jQuery and MediaElement.js (HTML5 Flash/Silverlight wrapper)
  *
- * Copyright 2010, John Dyer (http://johndyer.me)
+ * Copyright 2010-2011, John Dyer (http://j.hn/)
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
  */
