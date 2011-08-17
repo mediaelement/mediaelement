10dcf05d46ce1995f6dda3fcae418f06d025c185
johndyer <johndyer@gmail.com>
2011-08-17 15:16:06 -0500

    - reorg to use $.extend

--- src/js/mep-feature-fullscreen.js
+++ src/js/mep-feature-fullscreen.js
@@ -1,109 +1,131 @@
 (function($) {
-	mejs.MediaElementDefaults.forcePluginFullScreen = false;
 	
-	MediaElementPlayer.prototype.isFullScreen = false;
-	MediaElementPlayer.prototype.buildfullscreen = function(player, controls, layers, media) {
+	$.extend(mejs.MepDefaults, {
+		forcePluginFullScreen: false
+	});
+	
+	$.extend(MediaElementPlayer.prototype, {
+		
+		isFullScreen: false,
+		
+		docStyleOverflow: null,
+		
+		buildfullscreen: function(player, controls, layers, media) {
 
-		if (!player.isVideo)
-			return;
-			
-		// native events
-		if (mejs.MediaFeatures.hasNativeFullScreen) {
-			player.container.bind('webkitfullscreenchange', function(e) {
-			
-				if (document.webkitIsFullScreen) {
-					// reset the controls once we are fully in full screen
-					player.setControlsSize();
-				} else {				
-					// when a user presses ESC
-					// make sure to put the player back into place								
-					player.exitFullScreen();				
-				}
-			});
-		}
-
-		var 			
-			normalHeight = 0,
-			normalWidth = 0,
-			container = player.container,
-			docElement = document.documentElement,
-			docStyleOverflow,
-			parentWindow = window.parent,
-			parentiframes,			
-			iframe,
-			fullscreenBtn = 
-				$('<div class="mejs-button mejs-fullscreen-button"><button type="button"></button></div>')
-				.appendTo(controls)
-				.click(function() {
-					var isFullScreen = (mejs.MediaFeatures.hasNativeFullScreen) ?
-									document.webkitIsFullScreen :
-									player.isFullScreen;													
-					
-					if (isFullScreen) {
-						player.exitFullScreen();
-					} else {						
-						player.enterFullScreen();
+			if (!player.isVideo)
+				return;
+				
+			// native events
+			if (mejs.MediaFeatures.hasNativeFullScreen) {
+				player.container.bind('webkitfullscreenchange', function(e) {
+				
+					if (document.webkitIsFullScreen) {
+						// reset the controls once we are fully in full screen
+						player.setControlsSize();
+					} else {				
+						// when a user presses ESC
+						// make sure to put the player back into place								
+						player.exitFullScreen();				
 					}
 				});
-		
-		player.enterFullScreen = function() {
+			}
+
+			var 			
+				normalHeight = 0,
+				normalWidth = 0,
+				container = player.container,						
+				fullscreenBtn = 
+					$('<div class="mejs-button mejs-fullscreen-button"><button type="button"></button></div>')
+					.appendTo(controls)
+					.click(function() {
+						var isFullScreen = (mejs.MediaFeatures.hasNativeFullScreen) ?
+										document.webkitIsFullScreen :
+										player.isFullScreen;													
+						
+						if (isFullScreen) {
+							player.exitFullScreen();
+						} else {						
+							player.enterFullScreen();
+						}
+					});
+			
+			player.fullscreenBtn = fullscreenBtn;
+
+			
+			$(window).bind('resize',function (e) {
+				player.setControlsSize();
+			});		
+
+			$(document).bind('keydown',function (e) {
+				if (player.isFullScreen && e.keyCode == 27) {
+					player.exitFullScreen();
+				}
+			});
+				
+		},
+		enterFullScreen: function() {
+			
+			var t = this;
 			
 			// firefox can't adjust plugin sizes without resetting :(
-			if (player.pluginType !== 'native' && (mejs.MediaFeatures.isFirefox || player.options.forcePluginFullScreen)) {
-				media.setFullscreen(true);
+			if (t.media.pluginType !== 'native' && (mejs.MediaFeatures.isFirefox || t.options.forcePluginFullScreen)) {
+				t.media.setFullscreen(true);
 				//player.isFullScreen = true;
 				return;
 			}		
 			
 			// attempt to set fullscreen
 			if (mejs.MediaFeatures.hasNativeFullScreen) {
-				player.container[0].webkitRequestFullScreen();									
+				t.container[0].webkitRequestFullScreen();									
 			}
 								
 			// store overflow 
-			docStyleOverflow = docElement.style.overflow;
+			docStyleOverflow = document.documentElement.style.overflow;
 			// set it to not show scroll bars so 100% will work
-			docElement.style.overflow = 'hidden';				
+			document.documentElement.style.overflow = 'hidden';				
 		
 			// store
-			normalHeight = player.container.height();
-			normalWidth = player.container.width();
+			normalHeight = t.container.height();
+			normalWidth = t.container.width();
 
 			// make full size
-			container
+			t.container
 				.addClass('mejs-container-fullscreen')
 				.width('100%')
 				.height('100%')
 				.css('z-index', 1000);
 				//.css({position: 'fixed', left: 0, top: 0, right: 0, bottom: 0, overflow: 'hidden', width: '100%', height: '100%', 'z-index': 1000});				
 				
-			if (player.pluginType === 'native') {
-				player.$media
+			if (t.pluginType === 'native') {
+				t.$media
 					.width('100%')
 					.height('100%');
 			} else {
-				container.find('object embed')
+				t.container.find('object embed')
 					.width('100%')
 					.height('100%');
-				player.media.setVideoSize($(window).width(),$(window).height());
+				t.media.setVideoSize($(window).width(),$(window).height());
 			}
 			
-			layers.children('div')
+			t.layers.children('div')
 				.width('100%')
 				.height('100%');
 
-			fullscreenBtn
+			t.fullscreenBtn
 				.removeClass('mejs-fullscreen')
 				.addClass('mejs-unfullscreen');
 
-			player.setControlsSize();
-			player.isFullScreen = true;
-		};
-		player.exitFullScreen = function() {
-
+			t.setControlsSize();
+			t.isFullScreen = true;
+		},
+		
+		exitFullScreen: function() {
+			
+			var t = this;		
+		
 			// firefox can't adjust plugins
-			if (player.pluginType !== 'native' && mejs.MediaFeatures.isFirefox) {				
-				media.setFullscreen(false);
+			if (t.media.pluginType !== 'native' && mejs.MediaFeatures.isFirefox) {				
+				t.media.setFullscreen(false);
 				//player.isFullScreen = false;
 				return;
 			}		
@@ -114,49 +136,38 @@
 			}	
 
 			// restore scroll bars to document
-			docElement.style.overflow = docStyleOverflow;					
+			document.documentElement.style.overflow = docStyleOverflow;					
 				
-			container
+			t.container
 				.removeClass('mejs-container-fullscreen')
 				.width(normalWidth)
 				.height(normalHeight)
 				.css('z-index', 1);
 				//.css({position: '', left: '', top: '', right: '', bottom: '', overflow: 'inherit', width: normalWidth + 'px', height: normalHeight + 'px', 'z-index': 1});
 			
-			if (player.pluginType === 'native') {
-				player.$media
+			if (t.pluginType === 'native') {
+				t.$media
 					.width(normalWidth)
 					.height(normalHeight);
 			} else {
-				container.find('object embed')
+				t.container.find('object embed')
 					.width(normalWidth)
 					.height(normalHeight);
 					
-				player.media.setVideoSize(normalWidth, normalHeight);
+				t.media.setVideoSize(normalWidth, normalHeight);
 			}				
 
-			layers.children('div')
+			t.layers.children('div')
 				.width(normalWidth)
 				.height(normalHeight);
 
-			fullscreenBtn
+			t.fullscreenBtn
 				.removeClass('mejs-unfullscreen')
 				.addClass('mejs-fullscreen');
 
-			player.setControlsSize();
-			player.isFullScreen = false;
-		};
-		
-		$(window).bind('resize',function (e) {
-			player.setControlsSize();
-		});		
-
-		$(document).bind('keydown',function (e) {
-			if (player.isFullScreen && e.keyCode == 27) {
-				player.exitFullScreen();
-			}
-		});
-			
-	}
+			t.setControlsSize();
+			t.isFullScreen = false;
+		}	
+	});
 
 })(mejs.$);
--- src/js/mep-feature-playpause.js
+++ src/js/mep-feature-playpause.js
@@ -1,37 +1,39 @@
 (function($) {
 	// PLAY/pause BUTTON
-	MediaElementPlayer.prototype.buildplaypause = function(player, controls, layers, media) {
-		var play = 
-			$('<div class="mejs-button mejs-playpause-button mejs-play" type="button">' +
-				'<button type="button"></button>' +
-			'</div>')
-			.appendTo(controls)
-			.click(function(e) {
-				e.preventDefault();
-			
-				if (media.paused) {
-					media.play();
-				} else {
-					media.pause();
-				}
+	$.extend(MediaElementPlayer.prototype, {
+		buildplaypause: function(player, controls, layers, media) {
+			var play = 
+				$('<div class="mejs-button mejs-playpause-button mejs-play" type="button">' +
+					'<button type="button"></button>' +
+				'</div>')
+				.appendTo(controls)
+				.click(function(e) {
+					e.preventDefault();
 				
-				return false;
-			});
+					if (media.paused) {
+						media.play();
+					} else {
+						media.pause();
+					}
+					
+					return false;
+				});
 
-		media.addEventListener('play',function() {
-			play.removeClass('mejs-play').addClass('mejs-pause');
-		}, false);
-		media.addEventListener('playing',function() {
-			play.removeClass('mejs-play').addClass('mejs-pause');
-		}, false);
+			media.addEventListener('play',function() {
+				play.removeClass('mejs-play').addClass('mejs-pause');
+			}, false);
+			media.addEventListener('playing',function() {
+				play.removeClass('mejs-play').addClass('mejs-pause');
+			}, false);
 
 
-		media.addEventListener('pause',function() {
-			play.removeClass('mejs-pause').addClass('mejs-play');
-		}, false);
-		media.addEventListener('paused',function() {
-			play.removeClass('mejs-pause').addClass('mejs-play');
-		}, false);
-	}
+			media.addEventListener('pause',function() {
+				play.removeClass('mejs-pause').addClass('mejs-play');
+			}, false);
+			media.addEventListener('paused',function() {
+				play.removeClass('mejs-pause').addClass('mejs-play');
+			}, false);
+		}
+	});
 	
 })(mejs.$);
