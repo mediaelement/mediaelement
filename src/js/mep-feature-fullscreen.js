f63b7d4b103548fcd871ddf8f890f469ce4f36c1
gavinlynch <gavin.b.lynch@gmail.com>
2011-11-27 16:13:38 -0600

    Attempting to make Fullscreen mostly work for YouTube. Sorry Firefox!

--- src/js/mep-feature-fullscreen.js
+++ src/js/mep-feature-fullscreen.js
@@ -78,7 +78,7 @@
 			
 			
 			// firefox+flash can't adjust plugin sizes without resetting :(
-			if (t.container.find('object,embed').length > 0 && (mejs.MediaFeatures.isGecko || t.options.forcePluginFullScreen)) {
+			if (t.container.find('object,embed,iframe').length > 0 && (mejs.MediaFeatures.isGecko || t.options.forcePluginFullScreen)) {
 				t.media.setFullscreen(true);
 				//player.isFullScreen = true;
 				return;
@@ -154,7 +154,10 @@
 				t.container.find('object, embed, iframe')
 					.width('100%')
 					.height('100%');
-				t.media.setVideoSize($(window).width(),$(window).height());
+					
+				if (!mejs.MediaFeatures.hasTrueNativeFullScreen) {
+					t.media.setVideoSize($(window).width(),$(window).height());
+				}
 			}
 			
 			t.layers.children('div')
