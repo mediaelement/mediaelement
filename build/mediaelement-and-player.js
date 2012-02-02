1ead35c7eaffed2fcb059ad57b6b31e30cc12c6b
John Dyer <johndyer@gmail.com>
2012-02-02 16:51:10 -0600

    2.6.5 merged

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -1716,7 +1716,8 @@ if (typeof jQuery != 'undefined') {
 				t.$media.attr('controls', 'controls');
 
 				// attempt to fix iOS 3 bug
-				t.$media.removeAttr('poster');
+				//t.$media.removeAttr('poster');
+                                // no Issue found on iOS3 -ttroxell
 
 				// override Apple's autoplay override for iPads
 				if (mf.isiPad && t.media.getAttribute('autoplay') !== null) {
@@ -2539,6 +2540,7 @@ if (typeof jQuery != 'undefined') {
 	window.MediaElementPlayer = mejs.MediaElementPlayer;
 
 })(mejs.$);
+
 (function($) {
 
 	$.extend(mejs.MepDefaults, {
