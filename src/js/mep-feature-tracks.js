bac9a5465f6ba1bbf72c71cc7e5e9c0576fed2c0
Marko Marković <okram@civokram.com>
2012-05-25 16:45:47 +0200

    Fix play/pause when clicking on video when overlays are displayed
    If the subtitles or chapters are enabled and shown, you're unable to click
    on video to pause it.

--- src/js/mep-feature-tracks.js
+++ src/js/mep-feature-tracks.js
@@ -117,7 +117,7 @@
 					// chapters
 					if (player.hasChapters) {
 						player.chapters.css('visibility','visible');
-						player.chapters.fadeIn(200);
+						player.chapters.fadeIn(200).height(player.chapters.find('.mejs-chapter').outerHeight());
 					}
 				},
 				function () {
@@ -258,7 +258,7 @@
 				for (i=0; i<track.entries.times.length; i++) {
 					if (t.media.currentTime >= track.entries.times[i].start && t.media.currentTime <= track.entries.times[i].stop){
 						t.captionsText.html(track.entries.text[i]);
-						t.captions.show();
+						t.captions.show().height(0);
 						return; // exit out if one is visible;
 					}
 				}
