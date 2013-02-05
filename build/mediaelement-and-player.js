2598a7d279cb3b305dc373143739e674d9608430
Aurélien Matouillot <a.matouillot@gmail.com>
2013-02-05 14:10:50 +0100

    rebuild

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -2059,7 +2059,7 @@ if (typeof jQuery != 'undefined') {
 					t.media.play();
 				}
 					
-			} else if (mf.isAndroid && t.AndroidUseNativeControls) {
+			} else if (mf.isAndroid && t.options.AndroidUseNativeControls) {
 				
 				// leave default player
 
