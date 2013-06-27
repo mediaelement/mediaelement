c753e35e2b8aaa7ac0d8f165e9a2614b2f855f7b
Peter Hentges <peter.hentges@capella.edu>
2013-06-27 14:50:23 -0500

    Fix for issue #902
    In build tracks function, I added a check to see if the browser will
    attempt to do present captions natively. (Currently only a feature in
    Chrome. Outside of mejs, my if statement would include a check to see
    if any textTracks were declared (t.domNode.textTracks.length > 0), but
    the buildtracks function already checks for player.tracks.length.) If
    this is true, we prefer to use mejs captions and loop through the
    textTracks to set their mode to "hidden" to prevent display.

--- src/js/mep-feature-tracks.js
+++ src/js/mep-feature-tracks.js
@@ -29,6 +29,11 @@
 				i, 
 				options = '';
 
+			if (t.domNode.textTracks) { // if browser will do native captions, prefer mejs captions, loop through tracks and hide
+				for (var i = t.domNode.textTracks.length - 1; i >= 0; i--) {
+					t.domNode.textTracks[i].mode = "hidden";
+				}
+			}
 			player.chapters = 
 					$('<div class="mejs-chapters mejs-layer"></div>')
 						.prependTo(layers).hide();
