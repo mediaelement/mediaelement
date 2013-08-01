deb25dcf34a925d3d813fb8e48fd64efebf76ef4
John Dyer <johndyer@gmail.com>
2013-07-31 22:16:21 -0500

    UPdating to 2.13.0 for Wordpress release.

--- src/js/me-namespace.js
+++ src/js/me-namespace.js
@@ -2,7 +2,7 @@
 var mejs = mejs || {};
 
 // version number
-mejs.version = '2.12.1'; 
+mejs.version = '2.13.0'; 
 console.log('ME.js version', mejs.version);
 
 // player number (for missing, same id attr)
