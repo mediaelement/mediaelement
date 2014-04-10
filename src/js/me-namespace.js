cb415db2b46b9b61ae5002712fd2bbb244adc6b7
John Dyer <johndyer@gmail.com>
2014-04-10 09:56:22 -0500

    version

--- src/js/me-namespace.js
+++ src/js/me-namespace.js
@@ -2,7 +2,7 @@
 var mejs = mejs || {};
 
 // version number
-mejs.version = '2.14.1'; 
+mejs.version = '2.14.2'; 
 console.log('ME.js version', mejs.version);
 
 // player number (for missing, same id attr)
