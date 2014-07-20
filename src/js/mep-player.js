9432931822393762dc36f2ce2af8cea36ba1e1d2
Wizard13 <Wizard13@users.noreply.github.com>
2014-07-20 23:56:01 +0300

    Fix wrong initial player size when responsive

--- src/js/mep-player.js
+++ src/js/mep-player.js
@@ -777,8 +777,8 @@
 
 				// do we have the native dimensions yet?
 				var
-					nativeWidth = t.isVideo ? ((t.media.videoWidth && t.media.videoWidth > 0) ? t.media.videoWidth : t.options.defaultVideoWidth) : t.options.defaultAudioWidth,
-					nativeHeight = t.isVideo ? ((t.media.videoHeight && t.media.videoHeight > 0) ? t.media.videoHeight : t.options.defaultVideoHeight) : t.options.defaultAudioHeight,
+					nativeWidth = t.isVideo ? ((t.media.videoWidth && t.media.videoWidth > 0) ? t.media.videoWidth : (t.media.getAttribute('width') !== null ? t.media.getAttribute('width') : t.options.defaultVideoWidth)) : t.options.defaultAudioWidth,
+					nativeHeight = t.isVideo ? ((t.media.videoHeight && t.media.videoHeight > 0) ? t.media.videoHeight : (t.media.getAttribute('height') !== null ? t.media.getAttribute('height') : t.options.defaultVideoHeight)) : t.options.defaultAudioHeight,
 					parentWidth = t.container.parent().closest(':visible').width(),
 					newHeight = t.isVideo || !t.options.autosizeProgress ? parseInt(parentWidth * nativeHeight/nativeWidth, 10) : nativeHeight;
 
