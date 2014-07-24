894a4e61d9cb48282e02f1ec85c628b79775265e
Matthew Hillman <mhillman@bomgar.com>
2014-07-24 16:47:10 -0500

    Revert "Improvements to speed feature:"
    This reverts commit e9efc9473ca38c240b712a11ba4c035651c204d4.
    
    Remove these changes from this branch

--- src/js/mep-feature-speed.js
+++ src/js/mep-feature-speed.js
@@ -5,6 +5,8 @@
 
 		speeds: ['1.50', '1.25', '1.00', '0.75'],
 
+		speedStyle: {'1.50': 'mejs-speed-150', '1.25': 'mejs-speed-125', '1.00': 'mejs-speed-100', '0.75': 'mejs-speed-075'},
+
 		defaultSpeed: '1.00'
 
 	});
@@ -18,17 +20,8 @@
 			var t = this;
 
 			if (t.media.pluginType == 'native') {
-				var s = '<div class="mejs-button mejs-speed-button"><button type="button">'+t.options.defaultSpeed+'x</button><div class="mejs-speed-selector"><ul>';
-				var i, ss;
-
-				if ($.inArray(t.options.defaultSpeed, t.options.speeds) === -1) {
-					t.options.speeds.push(t.options.defaultSpeed);
-				}
-
-				t.options.speeds.sort(function(a, b) {
-					return parseFloat(b) - parseFloat(a);
-				});
-
+				var s = '<div class="mejs-button mejs-speed-button mejs-speed-100"><button type="button"></button><div class="mejs-speed-selector"><ul>';
+				var i;
 				for (i = 0; i < t.options.speeds.length; i++) {
 					s += '<li><input type="radio" name="speed" value="' + t.options.speeds[i] + '" id="' + t.options.speeds[i] + '" ';
 					if (t.options.speeds[i] == t.options.defaultSpeed) {
@@ -42,20 +35,27 @@
 
 				player.speedButton = $(s).appendTo(controls);
 
-				player.playbackspeed = t.options.defaultSpeed;
-
-				player.speedButton
+				player.speedButton.hover(function() {
+					$(this).find('.mejs-speed-selector').css('visibility', 'visible');
+				}, function() {
+					$(this).find('.mejs-speed-selector').css('visibility', 'hidden');
+				})
 				.on('click', 'input[type=radio]', function() {
+					var lastSpeed = t.options.speedStyle[player.playbackspeed];
 					player.playbackspeed = $(this).attr('value');
-					media.playbackRate = parseFloat(player.playbackspeed);
-					player.speedButton.find('button').text(player.playbackspeed + 'x');
+					media.playbackRate = parseFloat($(this).attr('value'));
+					player.speedButton.removeClass(lastSpeed).addClass(t.options.speedStyle[player.playbackspeed]);
 					player.speedButton.find('.mejs-speed-selected').removeClass('mejs-speed-selected');
 					player.speedButton.find('input[type=radio]:checked').next().addClass('mejs-speed-selected');
 				});
 
-				ss = player.speedButton.find('.mejs-speed-selector');
-				ss.height(this.speedButton.find('.mejs-speed-selector ul').outerHeight(true) + player.speedButton.find('.mejs-speed-translations').outerHeight(true));
-				ss.css('top', (-1 * ss.height()) + 'px');
+				player.speedButton.find('li').hover(function() {
+					$(this).css('background-color', 'linear-gradient(rgba(44,124,145,0.8), rgba(78,183,212,0.8))');
+				}, function() {
+					$(this).css('background-color', 'rgba(50,50,50,0)');
+				});
+
+				player.speedButton.find('.mejs-speed-selector').height(this.speedButton.find('.mejs-speed-selector ul').outerHeight(true) + player.speedButton.find('.mejs-speed-translations').outerHeight(true));
 			}
 		}
 	});
