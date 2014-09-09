14675e6006ae7eb2768eadd36fb3588bb96724a7
Maksim Chemerisuk <chemerisuk@users.noreply.github.com>
2014-09-09 16:11:50 +0300

    Fix IE8 error on window resizing

--- src/js/mep-player.js
+++ src/js/mep-player.js
@@ -827,7 +827,7 @@
 					newHeight = $(window).height();
 				}
 
-				if ( newHeight != 0 && parentWidth != 0 ) {
+				if ( newHeight && parentWidth ) {
 					// set outer container size
 					t.container
 						.width(parentWidth)
