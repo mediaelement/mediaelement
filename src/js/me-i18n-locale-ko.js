256130eaf3de6901efdacf55bcea187d70062fa3
Olivier Jaquemet <olivier.jaquemet@gmail.com>
2014-09-18 13:12:02 +0200

    i18n : add spanish, italian, japanese, korean and portuguese translations
    Adding new translation in spanish, italian, japanese, korean and
    portuguese, for all strings from mediaelement-and-player.js

---
+++ src/js/me-i18n-locale-ko.js
@@ -0,0 +1,36 @@
+/*!
+ * This is a i18n.locale language object.
+ *
+ * Korean translation by Jalios, Twitter: @Jalios
+ *
+ * @author
+ *   Jalios (Twitter: @Jalios)
+ *
+ * @see
+ *   me-i18n.js
+ *
+ * @params
+ *  - exports - CommonJS, window ..
+ */
+;(function(exports, undefined) {
+
+    "use strict";
+
+    if (typeof exports.ko === 'undefined') {
+        exports.ko = {
+            "None" : "없음",
+            "Unmute" : "음소거 해제",
+            "Fullscreen" : "전체화면",
+            "Download File" : "파일 다운로드",
+            "Mute Toggle" : "음소거 전환",
+            "Play/Pause" : "작동/정지",
+            "Captions/Subtitles" : "캡션/자막",
+            "Download Video" : "비디오 다운로드",
+            "Mute" : "말 없는",
+            "Turn off Fullscreen" : "전체화면 해제",
+            "Go Fullscreen" : "전체화면 가기",
+            "Close" : "종료"
+        };
+    }
+
+}(mejs.i18n.locale.strings));
