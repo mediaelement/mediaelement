7875e59b05e2ea5c12fa9a94c602719328fb05a8
Adam Blankenship <i.adambear@gmail.com>
2015-03-26 21:52:58 -0700

    Improved build & styling

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,99 +3,117 @@
  * Configure grunt concat
  */
 
+/* Configure player vars before task is initiated to prevent the need
+ for changing the features of used in standard & i18n separately.
+ */
+var me = {};
+
+me.player = {
+  universal: [
+    'mep-library.js'
+  ],
+  variable: [
+    /* Switch source for i18n support*/
+    'mep-player.js',
+    'mep-feature-contextmenu.js',
+    'mep-feature-fullscreen.js',
+    'mep-feature-playpause.js',
+    'mep-feature-progress.js',
+    'mep-feature-time.js',
+    'mep-feature-volume.js'
+      //'mep-feature-postroll.js',
+      //'mep-feature-skipback.js',
+      //'mep-feature-tracks.js'
+  ],
+  features: [
+    //'mep-feature-ads-vast.js',
+    //'mep-feature-ads.js',
+    //'mep-feature-backlight.js',
+    //'mep-feature-endedhtml.js',
+    //'mep-feature-googleanalytics.js',
+    //'mep-feature-loop.js',
+    //'mep-feature-playlist.js',
+    //'mep-feature-sourcechooser.js',
+    //'mep-feature-speed.js',
+    //'mep-feature-stop.js',
+    //'mep-feature-universalgoogleanalytics.js',
+    //'mep-feature-visualcontrols.js',
+  ]
+};
+
+/* If a language file is present i18n sources will be used in the script. */
+me.languages = [
+  //'me-i18n-locale-de.js',
+  //'me-i18n-locale-es.js',
+  //'me-i18n-locale-fr.js',
+  //'me-i18n-locale-it.js',
+  //'me-i18n-locale-ja.js',
+  //'me-i18n-locale-ko.js',
+  //'me-i18n-locale-pt.js',
+  //'me-i18n-locale-zh-cn.js',
+  //'me-i18n-locale-zh.js'
+];
+
+me.base = {
+  universal: [
+    'me-header.js',
+    'me-namespace.js',
+    'me-utility.js',
+    'me-plugindetector.js',
+    'me-featuredetection.js',
+    'me-mediaelements.js'
+    // me-shim.js automatically appended.
+  ]
+};
+
+/* 
+ * DON'T EDIT BELOW THIS LINE
+ */
+me.i18n = me.languages.length ? 'src/js/i18n/' : 'src/js/';
+
+me.src = function (array, path) {
+  a = [];
+  for (var i in array) {
+    var script = path + array[i];
+    a.push(script);
+  }
+  return a;
+};
+
+me.base.scripts = function (path) {
+  var a = [],
+    u = me.src(me.base.universal, 'src/js/'),
+    l = me.languages,
+    shim = me.src(['me-shim.js'], path);
+
+  if (l.length) {
+    l.unshift('me-i18n.js');
+    l = me.src(l, path);
+  }
+
+  return a.concat(u, l, shim);
+};
+
+me.player.scripts = function (path) {
+  var a = [],
+    u = me.src(me.player.universal, 'src/js/'),
+    b = me.src(me.player.variable, path),
+    f = me.src(me.player.features, 'src/js/');
+
+  return a.concat(u, b, f);
+};
+
+me.base = me.base.scripts(me.i18n);
+me.player = me.player.scripts(me.i18n);
+
 module.exports = {
-  me: {
-    src: [
-      'src/js/me-header.js',
-      'src/js/me-namespace.js',
-      'src/js/me-utility.js',
-      'src/js/me-plugindetector.js',
-      'src/js/me-featuredetection.js',
-      'src/js/me-mediaelements.js',
-      'src/js/me-shim.js'
-    ],
+  base: {
+    src: me.base,
     dest: 'local-build/mediaelement.base.js'
   },
-  mei18n: {
-    src: [
-      'src/js/me-header.js',
-      'src/js/me-namespace.js',
-      'src/js/me-utility.js',
-      'src/js/me-plugindetector.js',
-      'src/js/me-featuredetection.js',
-      'src/js/me-mediaelements.js',
-      'src/js/i18n/me-i18n.js',
-      'src/js/i18n/me-shim.js'
-        // Bug #1263
-        //'src/js/i18n/me-i18n-locale-de.js',
-        //'src/js/i18n/me-i18n-locale-es.js',
-        //'src/js/i18n/me-i18n-locale-fr.js',
-        //'src/js/i18n/me-i18n-locale-it.js',
-        //'src/js/i18n/me-i18n-locale-ja.js',
-        //'src/js/i18n/me-i18n-locale-ko.js',
-        //'src/js/i18n/me-i18n-locale-pt.js',
-        //'src/js/i18n/me-i18n-locale-zh-cn.js',
-        //'src/js/i18n/me-i18n-locale-zh.js'
-    ],
-    dest: 'local-build/mediaelement.base.i18n.js'
-  },
-  meplayer: {
-    src: [
-      'src/js/mep-header.js',
-      'src/js/mep-library.js',
-      'src/js/mep-player.js',
-      'src/js/mep-feature-playpause.js',
-      'src/js/mep-feature-contextmenu.js',
-      'src/js/mep-feature-progress.js',
-      'src/js/mep-feature-time.js',
-      'src/js/mep-feature-volume.js',
-      'src/js/mep-feature-fullscreen.js'
-      //'src/js/mep-feature-ads-vast.js',
-      //'src/js/mep-feature-ads.js',
-      //'src/js/mep-feature-backlight.js',
-      //'src/js/mep-feature-endedhtml.js',
-      //'src/js/mep-feature-googleanalytics.js',
-      //'src/js/mep-feature-loop.js',
-      //'src/js/mep-feature-playlist.js',
-      //'src/js/mep-feature-postroll.js'
-      //'src/js/mep-feature-sourcechooser.js',
-      //'src/js/mep-feature-speed.js',
-      //'src/js/mep-feature-stop.js',
-      //'src/js/mep-feature-tracks.js',
-      //'src/js/mep-feature-skipback.js',
-      //'src/js/mep-feature-universalgoogleanalytics.js',
-      //'src/js/mep-feature-visualcontrols.js',
-    ],
+  player: {
+    src: me.player,
     dest: 'local-build/mediaelement.player.js'
-  },  
-  meplayeri18n: {
-    src: [
-      'src/js/mep-header.js',
-      'src/js/mep-library.js',
-      'src/js/i18n/mep-player.js',
-      'src/js/i18n/mep-feature-contextmenu.js',
-      'src/js/i18n/mep-feature-fullscreen.js',
-      'src/js/i18n/mep-feature-playpause.js',
-      'src/js/i18n/mep-feature-progress.js',
-      'src/js/mep-feature-time.js',
-      'src/js/i18n/mep-feature-volume.js'
-      //'src/js/i18n/mep-feature-postroll.js'
-      //'src/js/i18n/mep-feature-skipback.js',
-      //'src/js/i18n/mep-feature-tracks.js',
-      //'src/js/mep-feature-ads-vast.js',
-      //'src/js/mep-feature-ads.js',
-      //'src/js/mep-feature-backlight.js',
-      //'src/js/mep-feature-endedhtml.js',
-      //'src/js/mep-feature-googleanalytics.js',
-      //'src/js/mep-feature-loop.js',
-      //'src/js/mep-feature-playlist.js',
-      //'src/js/mep-feature-sourcechooser.js',
-      //'src/js/mep-feature-speed.js',
-      //'src/js/mep-feature-stop.js',
-      //'src/js/mep-feature-universalgoogleanalytics.js',
-      //'src/js/mep-feature-visualcontrols.js',
-    ],
-    dest: 'local-build/mediaelement.player.i18n.js'
   },
   bundle: {
     src: [
@@ -103,12 +121,5 @@ module.exports = {
       'local-build/mediaelement.player.js'
     ],
     dest: 'local-build/mediaelement.js'
-  },
-  bundlei18n: {
-    src: [
-      'local-build/mediaelement.base.i18n.js',
-      'local-build/mediaelement.player.i18n.js'
-    ],
-    dest: 'local-build/mediaelement.i18n.js'
   }
 };
--- grunt/uglify.js
+++ grunt/uglify.js
@@ -4,39 +4,23 @@
  */
 
 module.exports = {
-    options: {
-      preserveComments: "some",
-      compress: true,
-      mangle: true
-    },
-    minme: {
-      src: ['local-build/mediaelement.base.js'],
-      dest: 'local-build/mediaelement.base.min.js',
-      banner: 'src/js/me-header.js'
-    },
-    minmep: {
-      src: ['local-build/mediaelement.player.js'],
-      dest: 'local-build/mediaelement.player.min.js',
-      banner: 'src/js/mep-header.js'
-    },
-    minbundle: {
-      src: ['local-build/mediaelement.js'],
-      dest: 'local-build/mediaelement.min.js'
-    },
-    // I18n
-    minmei18n: {
-      src: ['local-build/mediaelement.base.i18n.js'],
-      dest: 'local-build/mediaelement.base.i18n.min.js',
-      banner: 'src/js/me-header.js'
-    },
-    minmepi18n: {
-      src: ['local-build/mediaelement.player.i18n.js'],
-      dest: 'local-build/mediaelement.player.i18n.min.js',
-      banner: 'src/js/mep-header.js'
-    },
-    minbundlei18n: {
-      src: ['local-build/mediaelement.i18n.js'],
-      dest: 'local-build/mediaelement.i18n.min.js'
-    }
-  
+  options: {
+    preserveComments: "some",
+    compress: true,
+    mangle: true
+  },
+  base: {
+    src: ['local-build/mediaelement.base.js'],
+    dest: 'local-build/mediaelement.base.min.js',
+    banner: 'src/js/me-header.js'
+  },
+  player: {
+    src: ['local-build/mediaelement.player.js'],
+    dest: 'local-build/mediaelement.player.min.js',
+    banner: 'src/js/mep-header.js'
+  },
+  bundle: {
+    src: ['local-build/mediaelement.js'],
+    dest: 'local-build/mediaelement.min.js'
+  }
 };
