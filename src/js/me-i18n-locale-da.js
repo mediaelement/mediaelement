e9a3cf9c28494e6fb95c835dc8450063a5f0c250
Jesper Skytte Hansen <jesper@skytte.it>
2015-10-09 08:56:40 +0200

    Add Danish locale
    I've created a Danish locale of mediaelement to be used on my own
    website. If others can use it, please do so.

---
+++ src/js/me-i18n-locale-da.js
@@ -0,0 +1,36 @@
+/*
+ * This is a i18n.locale language object.
+ *
+ * German translation by Jalios, Twitter: @Jalios
+ *
+ * @author
+ *   Jalios (Twitter: @Jalios)
+ *
+ * @see
+ *   me-i18n.js
+ *
+ * @params
+ *  - exports - CommonJS, window ..
+ */
+;(function(exports, undefined) {
+
+    "use strict";
+
+    if (typeof exports.da === 'undefined') {
+        exports.da = {
+            "None" : "Ingen",
+            "Unmute" : "Slå Mute Fra",
+            "Fullscreen" : "Fuld skærm",
+            "Download File" : "Hent Fil",
+            "Mute Toggle" : "Mute Til/Fra",
+            "Play/Pause" : "Afspil/Pause",
+            "Captions/Subtitles" : "Billedtekster/Undertekster",
+            "Download Video" : "Hent Video",
+            "Mute" : "Slå Mute Til",
+            "Turn off Fullscreen" : "Slå Fuld Skærm fra",
+            "Go Fullscreen" : "Gå til Fuld Skærm",
+            "Close" : "Luk"
+        };
+    }
+
+}(mejs.i18n.locale.strings));
