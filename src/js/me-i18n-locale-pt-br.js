bf042d2f6339357707f0ea503e007de21edf2175
Armando Meziat <amf-commits@mezi.at>
2015-11-17 23:49:06 -0200

    Brazilian Portuguese translation

---
+++ src/js/me-i18n-locale-pt-br.js
@@ -0,0 +1,38 @@
+/*!
+ * This is a i18n.locale language object.
+ *
+ * Brazilian Portuguese translation by Armando Meziat
+ *
+ * @author
+ *   Armando Meziat 
+ *   - Twitter: @odnamrataizem
+ *   - GitHub: https://github.com/odnamrataizem/
+ *
+ * @see
+ *   me-i18n.js
+ *
+ * @params
+ *  - exports - CommonJS, window ..
+ */
+;(function(exports, undefined) {
+
+    "use strict";
+
+    if (typeof exports['pt-BR'] === 'undefined') {
+        exports.pt = {
+            "None" : "Sem legendas",
+            "Unmute" : "Tirar silêncio",
+            "Fullscreen" : "Tela inteira",
+            "Download File" : "Baixar arquivo",
+            "Mute Toggle" : "Alternar silêncio",
+            "Play/Pause" : "Reproduzir/Pausar",
+            "Captions/Subtitles" : "Legendas",
+            "Download Video" : "Baixar vídeo",
+            "Mute" : "Silenciar",
+            "Turn off Fullscreen" : "Sair da tela inteira",
+            "Go Fullscreen" : "Ir para tela inteira",
+            "Close" : "Fechar"
+        };
+    }
+
+}(mejs.i18n.locale.strings));
