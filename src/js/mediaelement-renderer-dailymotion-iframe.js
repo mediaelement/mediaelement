0aa5ec5c41758621e90d6fe216a2aded16d31039
Rafael Miranda <rafa8626@gmail.com>
2016-09-19 09:38:02 -0400

    Added https protocol for all scripts/APIs to be loaded

--- src/js/mediaelement-renderer-dailymotion-iframe.js
+++ src/js/mediaelement-renderer-dailymotion-iframe.js
@@ -59,7 +59,7 @@
 			if (!this.isSDKStarted) {
 				var e = document.createElement('script');
 				e.async = true;
-				e.src = document.location.protocol + '//api.dmcdn.net/all.js';
+				e.src = 'https://api.dmcdn.net/all.js';
 				var s = document.getElementsByTagName('script')[0];
 				s.parentNode.insertBefore(e, s);
 				this.isSDKStarted = true;
