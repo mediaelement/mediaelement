a08054a834ee1b9e295b50b9d2c87283456ac8a4
Rafael Miranda <rafa8626@gmail.com>
2016-10-03 00:08:39 -0400

    Added catalan translation from #1746

---
+++ src/js/mediaelement-i18n-locale-ca.js
@@ -0,0 +1,42 @@
+/*!
+ * This is a i18n.locale language object.
+ *
+ * Catalan translation by Tongro
+ *
+ * @author
+ *   Tongro
+ *
+ * @see
+ *   me-i18n.js
+ *
+ * @params
+ *  - exports - CommonJS, window ..
+ */
+;(function (exports, undefined) {
+
+	"use strict";
+
+	if (typeof exports.ca === 'undefined') {
+		exports.ca = {
+			'mejs.ad-skip': "Salta publicitat",
+			'mejs.ad-skip-info': "Salta en %1 segons",
+			'mejs.audio-player': "Reproductor d'àudio",
+			'mejs.captions-subtitles': "Llegendes/Subtítols",
+			'mejs.close': "Tancar",
+			'mejs.download-file': "Descarregar arxiu",
+			'mejs.download-video': "Descarregar vídeo",
+			'mejs.fullscreen': "Pantalla completa",
+			'mejs.fullscreen-off': "Desconnectar pantalla completaa",
+			'mejs.fullscreen-on': "Anar a pantalla completa",
+			'mejs.mute': "Silenci",
+			'mejs.mute-toggle': "Alternar silenci",
+			'mejs.none': "Ningú",
+			'mejs.pause': "Pausa",
+			'mejs.play': "Reproducció",
+			'mejs.unmute': "Reactivar silenci",
+			'mejs.video-player': "Reproductor de vídeo",
+			'mejs.stop': 'Aturar'
+		};
+	}
+
+}(mejs.i18n.locale.strings));
