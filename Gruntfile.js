d321126ef5807872c20d1b07075eebdb51341411
Alex Bell <alex@bellandwhistle.net>
2016-10-12 07:45:18 -0700

    add joshing task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-copy');
@@ -25,6 +25,9 @@ module.exports = function(grunt) {
 	}
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['Gruntfile.js', 'src/js/**/*.js']
+		},
 		concat: {
 			me: {
 				src: [
@@ -205,10 +208,10 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.registerTask('default', ['concat', 'removelogging', 'uglify', 'cssmin', 'copy',
+	grunt.registerTask('default', ['jshint', 'concat', 'removelogging', 'uglify', 'cssmin', 'copy',
 		'shell:buildFlash', 'shell:buildFlashCDN', 'shell:buildFlashDebug', 'clean:temp']);
 
-    grunt.registerTask('html5only', ['concat', 'removelogging', 'uglify', 'cssmin', 'copy', 'clean:temp']);
-    grunt.registerTask('debug_html5', ['concat', 'uglify', 'cssmin', 'copy', 'clean:temp']);
+    grunt.registerTask('html5only', ['jshint', 'concat', 'removelogging', 'uglify', 'cssmin', 'copy', 'clean:temp']);
+    grunt.registerTask('debug_html5', ['jshint', 'concat', 'uglify', 'cssmin', 'copy', 'clean:temp']);
 
 };
