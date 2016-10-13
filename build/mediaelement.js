4fecfdf1279b433ad6966e632ddb4921abc4776a
Rafael Miranda <rmiranda@medscape.net>
2016-10-13 00:21:00 -0400

    Added one more MIME type for HLS videos

--- build/mediaelement.js
+++ build/mediaelement.js
@@ -1504,7 +1504,7 @@ mejs.version = '3.0-alpha';
 		 */
 		canPlayType: function (type) {
 
-			var mediaTypes = ['application/x-mpegURL', 'vnd.apple.mpegURL', 'audio/mpegURL', 'audio/hls', 'video/hls'];
+			var mediaTypes = ['application/x-mpegURL', 'application/x-mpegurl', 'vnd.apple.mpegURL', 'audio/mpegURL', 'audio/hls', 'video/hls'];
 			return mejs.MediaFeatures.hasMse && mediaTypes.indexOf(type) > -1;
 		},
 		/**
