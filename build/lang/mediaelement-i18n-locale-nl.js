a5738feae824cb7eab8adb3c4369d40c81952c62
Rafael Miranda <rmiranda@medscape.net>
2016-10-20 11:35:34 -0400

    Removed embed to favor object since it allows nested content; added warning message for Flash not enabled/installed and added translation string for it

--- build/lang/mediaelement-i18n-locale-nl.js
+++ build/lang/mediaelement-i18n-locale-nl.js
@@ -23,6 +23,7 @@
 
 			// me-shim
 			"mejs.download-file": "Bestand downloaden",
+			// "mejs.install-flash": "You are using a browser that does not have Flash player enabled or installed. Please turn on your Flash player plugin or download the latest version from https://get.adobe.com/flashplayer/",
 
 			// mep-feature-contextmenu
 			"mejs.fullscreen-off": "Volledig scherm uitschakelen",
