6ca8218fd81141d4df525edf808666f4210cfd1b
Rafael Miranda <rafa8626@gmail.com>
2016-10-21 11:15:55 -0400

    Compiled files to reflect latest changes

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -7017,7 +7017,7 @@ if (jQuery !== undefined) {
 				posterImg = posterDiv.find('img');
 
 			if (posterImg.length === 0) {
-				posterImg = $('<img width="100%" height="100%" alt="" />').appendTo(posterDiv);
+				posterImg = $('<img class="mejs-poster-img" width="100%" height="100%" alt="" />').appendTo(posterDiv);
 			}
 
 			posterImg.attr('src', url);
