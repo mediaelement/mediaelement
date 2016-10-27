f2b1884e28154f479c386b4929cbc0db285ea84e
Alex Bell <alex@bellandwhistle.net>
2016-10-26 23:13:51 -0700

    missing space.

--- src/js/mediaelementplayer-player.js
+++ src/js/mediaelementplayer-player.js
@@ -409,7 +409,7 @@
 				$('<span class="mejs-offscreen">' + videoPlayerTitle + '</span>').insertBefore(t.$media);
 				// build container
 				t.container =
-					$('<div id="' + t.id + '" class="mejs-container"' +
+					$('<div id="' + t.id + '" class="mejs-container" ' +
 						'tabindex="0" role="application" aria-label="' + videoPlayerTitle + '">' +
 						'<div class="mejs-inner">' +
 						'<div class="mejs-mediaelement"></div>' +
