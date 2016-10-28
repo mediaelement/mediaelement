f67454fedbf2efd462df67ec82467fdc92b0b46c
Rafael Miranda <rmiranda@medscape.net>
2016-10-27 21:07:40 -0400

    Added fix proposed by @FrankDaze in regards of number of digits compared between current time and new time when moving mouse

--- build/mediaelementplayer.js
+++ build/mediaelementplayer.js
@@ -1973,7 +1973,7 @@ if (jQuery !== undefined) {
 						newTime = (percentage <= 0.02) ? 0 : percentage * media.duration;
 
 						// seek to where the mouse is
-						if (mouseIsDown && newTime !== media.currentTime) {
+						if (mouseIsDown && newTime.toFixed(4) !== media.currentTime.toFixed(4)) {
 							media.setCurrentTime(newTime);
 						}
 
