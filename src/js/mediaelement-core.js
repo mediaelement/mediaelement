f049ee63242325ae5ab170ce9a4e409bf7de1f62
Rafael Miranda <rmiranda@medscape.net>
2016-11-07 08:25:03 -0500

    Removed unnecessary piece of code

--- src/js/mediaelement-core.js
+++ src/js/mediaelement-core.js
@@ -305,11 +305,6 @@
 				mediaElement.changeRenderer(renderInfo.rendererName, mediaFiles);
 
 				if (mediaElement.renderer === undefined || mediaElement.renderer === null) {
-
-					if (!mediaFiles[0].src) {
-						mediaElement.originalNode.removeAttribute('src');
-					}
-
 					event = doc.createEvent("HTMLEvents");
 					event.initEvent('error', false, false);
 					event.message = 'Error creating renderer';
