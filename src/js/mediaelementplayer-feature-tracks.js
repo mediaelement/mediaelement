fdae1ac16277980be9a7cab35bc7170390d17478
Jesse Baker <jesse.baker@elucidat.com>
2016-11-07 14:10:30 +0000

    Some small fixes to improve keyboard only navigation. Fixed issue where the subtitle track could not be changed using the arrow keys. Fixed issue where volume slider remained visible after tabbing to a different player control

--- src/js/mediaelementplayer-feature-tracks.js
+++ src/js/mediaelementplayer-feature-tracks.js
@@ -88,7 +88,7 @@
 			player.captionsButton =
 					$('<div class="mejs__button mejs__captions-button">'+
 						'<button type="button" aria-controls="' + t.id + '" title="' + tracksTitle + '" aria-label="' + tracksTitle + '"></button>'+
-						'<div class="mejs__captions-selector">'+
+						'<div class="mejs__captions-selector mejs__offscreen">'+
 							'<ul>'+
 								'<li>'+
 									'<input type="radio" name="' + player.id + '_captions" id="' + player.id + '_captions_none" value="none" checked="checked" />' +
@@ -121,19 +121,22 @@
 				});
 			} else {
 				// hover or keyboard focus
-				player.captionsButton.on( 'mouseenter focusin', function() {
-					$(this).find('.mejs__captions-selector').removeClass('mejs__offscreen');
-				})
-
-				// handle clicks to the language radio buttons
-				.on('click','input[type=radio]',function() {
-					lang = this.value;
-					player.setTrack(lang);
-				});
-
-				player.captionsButton.on( 'mouseleave focusout', function() {
-					$(this).find(".mejs__captions-selector").addClass("mejs__offscreen");
-				});
+				player.captionsButton
+					.on( 'mouseenter focusin', function() {
+						$(this).find('.mejs__captions-selector').removeClass('mejs__offscreen');
+					})
+					.on( 'mouseleave focusout', function() {
+						$(this).find(".mejs__captions-selector").addClass("mejs__offscreen");
+					})
+					// handle clicks to the language radio buttons
+					.on('click','input[type=radio]',function() {
+						lang = this.value;
+						player.setTrack(lang);
+					})
+					//Allow up/down arrow to change the selected radio without changing the volume.
+					.on('keyup keydown keypress', function(e) {
+						e.stopPropagation();
+					});
 
 			}
 
