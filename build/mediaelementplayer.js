fef45a792f05b4325de21d3f0c3f318535179d40
Rafael Miranda <rmiranda@medscape.net>
2016-11-08 15:38:23 -0500

    Added fix for issue described in #1924

--- build/mediaelementplayer.js
+++ build/mediaelementplayer.js
@@ -1547,7 +1547,10 @@ if (jQuery !== undefined) {
 		play: function () {
 			var t = this;
 
-			t.load();
+			// only load if the current time is 0 to ensure proper
+			if (t.media.getCurrentTime() <= 0) {
+				t.load();
+			}
 			t.media.play();
 		},
 		pause: function () {
