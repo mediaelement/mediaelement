1ef2c2d8527fe1f9d4bdee78e514f9dde2a87baf
Rafael Miranda <rafa8626@gmail.com>
2016-11-15 09:53:00 -0500

    Updated changelog and compiled files to reflect latest changes

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -7158,8 +7158,6 @@ if (jQuery !== undefined) {
 		remove: function () {
 			var t = this, featureIndex, feature;
 
-			t.container.prev('.mejs-offscreen').remove();
-
 			// invoke features cleanup
 			for (featureIndex in t.options.features) {
 				feature = t.options.features[featureIndex];
@@ -7196,6 +7194,7 @@ if (jQuery !== undefined) {
 			delete mejs.players[t.id];
 
 			if (typeof t.container === 'object') {
+				t.container.prev('.mejs-offscreen').remove();
 				t.container.remove();
 			}
 			t.globalUnbind();
