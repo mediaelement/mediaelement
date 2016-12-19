fe5967f44f519985da8d88151a6905dd8d883067
Rafael Miranda <rmiranda@medscape.net>
2016-12-19 12:41:03 -0500

    Added JS code to mimic fix for fullscreen rotation issue

--- src/js/mediaelementplayer-feature-fullscreen.js
+++ src/js/mediaelementplayer-feature-fullscreen.js
@@ -450,11 +450,19 @@
 			if (isNative) {
 				t.$media
 				.width('100%')
-				.height('100%');
+				.height('100%')
+				.css({
+					'min-width': '100%',
+					'min-height': '100%'
+				});
 			} else {
 				t.container.find('iframe, embed, object, video')
 				.width('100%')
-				.height('100%');
+				.height('100%')
+				.css({
+					'min-width': '100%',
+					'min-height': '100%'
+				});
 			}
 
 			if (t.options.setDimensions) {
@@ -514,11 +522,19 @@
 				if (isNative) {
 					t.$media
 						.width(t.normalWidth)
-						.height(t.normalHeight);
+						.height(t.normalHeight)
+						.css({
+							'min-width': 'initial',
+							'min-height': 'initial'
+						});
 				} else {
 					t.container.find('iframe, embed, object, video')
 						.width(t.normalWidth)
-						.height(t.normalHeight);
+						.height(t.normalHeight)
+						.css({
+							'min-width': 'initial',
+							'min-height': 'initial'
+						});
 				}
 
 				t.media.setSize(t.normalWidth, t.normalHeight);
