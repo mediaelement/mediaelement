996c7706fb0022adc5108ee4612dac4dba60630a
Rafael Miranda <rmiranda@medscape.net>
2016-12-19 12:34:50 -0500

    Added CSS styles to fix issues with mobile devices described on #1959 using stylesheet only

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -9271,13 +9271,11 @@ if (jQuery !== undefined) {
 			if (isNative) {
 				t.$media
 				.width('100%')
-				.height('100%')
-				.css('min-width','100%').css('min-height','100%');
+				.height('100%');
 			} else {
 				t.container.find('iframe, embed, object, video')
 				.width('100%')
-				.height('100%')
-				.css('min-width','100%').css('min-height','100%');
+				.height('100%');
 			}
 
 			if (t.options.setDimensions) {
@@ -9337,13 +9335,11 @@ if (jQuery !== undefined) {
 				if (isNative) {
 					t.$media
 						.width(t.normalWidth)
-						.height(t.normalHeight)
-						.css('min-width','initial').css('min-height','initial');
+						.height(t.normalHeight);
 				} else {
 					t.container.find('iframe, embed, object, video')
 						.width(t.normalWidth)
-						.height(t.normalHeight)
-						.css('min-width','initial').css('min-height','initial');
+						.height(t.normalHeight);
 				}
 
 				t.media.setSize(t.normalWidth, t.normalHeight);
