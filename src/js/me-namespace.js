b78c8e9416b1ba9d8be3de9417fbb7eda95a6b59
Rafael Miranda <rafa8626@gmail.com>
2017-01-06 06:59:53 -0500

    Release 2.23.5

--- src/js/me-namespace.js
+++ src/js/me-namespace.js
@@ -2,7 +2,7 @@
 var mejs = mejs || {};
 
 // version number
-mejs.version = '2.23.4';
+mejs.version = '2.23.5';
 console.log('ME.js version', mejs.version);
 
 // player number (for missing, same id attr)
