f7d08d9f4ee4645b8f00fe948231f115d54273d5
Rafael Miranda <rafa8626@gmail.com>
2017-01-10 00:09:30 -0500

    Removed conditional to just target infinite value

--- src/js/mediaelementplayer-feature-progress.js
+++ src/js/mediaelementplayer-feature-progress.js
@@ -289,14 +289,9 @@
 			// If media is does not have a finite duration, remove progress bar interaction
 			// and indicate that is a live broadcast
 			media.addEventListener('durationchange', function() {
-				if (media.duration !== Infinity && !isNaN(media.duration)) {
-					player.setProgressRail(e);
-					player.setCurrentRail(e);
-				} else {
-					if (!controls.find('.' + t.options.classPrefix + 'broadcast').length) {
-						controls.find('.' + t.options.classPrefix + 'time-rail').empty()
+				if (media.duration !== Infinity && !controls.find('.' + t.options.classPrefix + 'broadcast').length) {
+					controls.find('.' + t.options.classPrefix + 'time-rail').empty()
 						.html('<span class="' + t.options.classPrefix + 'broadcast">' + mejs.i18n.t('mejs.live-broadcast') + '</span>');
-					}
 				}
 			}, false);
 			media.addEventListener('progress', function (e) {
