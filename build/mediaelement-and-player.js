cf6930ddfbc156cd33623c5adda60bf0b035b048
Rafael Miranda <rafa8626@gmail.com>
2017-01-26 20:44:23 -0500

    Release 3.0.2

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -878,7 +878,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var mejs = {};
 
 // version number
-mejs.version = '3.0.1';
+mejs.version = '3.0.2';
 
 // Basic HTML5 settings
 mejs.html5media = {
