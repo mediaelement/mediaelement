995296d0abeacbeb56a415cad020ca6a492439e9
Petter <johansson.petter@gmail.com>
2017-02-24 10:55:20 +0100

    added Swedish language

--- src/js/languages/sv.js
+++ src/js/languages/sv.js
@@ -1,10 +1,22 @@
+'use strict';
+
+/*!
+ * This is a `i18n` language object.
+ *
+ * Swedish
+ *
+ * @author
+ *   Petter (Twitter: @petter_j)
+ *
+ * @see core/i18n.js
+ */
 (function(exports) {
     if (exports.sv === undefined) {
         exports.sv = {
             "mejs.plural-form": 1,
 
             // renderers/flash.js
-            "mejs.install-flash": "You are using a browser that does not have Flash player enabled or installed. Please turn on your Flash player plugin or download the latest version from https://get.adobe.com/flashplayer/",
+            "mejs.install-flash": "Du använder en webbläsare som inte har Flash Player aktiverat eller installerad. Aktivera Flash Player eller hämta den senaste versionen från https://get.adobe.com/flashplayer/",
 
             // features/contextmenu.js
             "mejs.fullscreen-off": "Stäng av Fullskärmläge",
@@ -15,112 +27,112 @@
             "mejs.fullscreen": "Fullskärm",
 
             // features/jumpforward.js
-            "mejs.time-jump-forward": ["Jump forward 1 second", "Jump forward %1 seconds"],
+            "mejs.time-jump-forward": ["Hoppa fram 1 sekund", "Hoppa fram %1 sekunder"],
 
             // features/loop.js
-            "mejs.loop": "Toggle Loop",
+            "mejs.loop": "Repetera",
 
             // features/playpause.js
-            "mejs.play": "Play",
-            "mejs.pause": "Pause",
+            "mejs.play": "Spela",
+            "mejs.pause": "Pausa",
 
             // features/postroll.js
-            "mejs.close": "Close",
+            "mejs.close": "Stäng",
 
             // features/progress.js
-            "mejs.time-slider": "Time Slider",
-            "mejs.time-help-text": "Use Left/Right Arrow keys to advance one second, Up/Down arrows to advance ten seconds.",
+            "mejs.time-slider": "Tidslinje",
+            "mejs.time-help-text": "Använd Vänster/Höger piltangent för att spola en sekund, Upp/Ner piltangent spola tio sekunder.",
 
             // features/skipback.js
-            "mejs.time-skip-back": ["Skip back 1 second", "Skip back %1 seconds"],
+            "mejs.time-skip-back": ["Hoppa tillbaka 1 sekund", "Hoppa tillbaka %1 sekunder"],
 
             // features/tracks.js
-            "mejs.captions-subtitles": "Captions/Subtitles",
-            "mejs.captions-chapters": "Chapters",
-            "mejs.none": "None",
+            "mejs.captions-subtitles": "Textning/Undertexter",
+            "mejs.captions-chapters": "Kapitel",
+            "mejs.none": "Ingen",
 
             // features/volume.js
-            "mejs.volume-help-text": "Use Up/Down Arrow keys to increase or decrease volume.",
-            "mejs.unmute": "Unmute",
-            "mejs.mute": "Mute",
-            "mejs.volume-slider": "Volume Slider",
+            "mejs.volume-help-text": "Använd Upp/Ner piltangent för att öka eller minska volymen.",
+            "mejs.unmute": "Ljud på",
+            "mejs.mute": "Ljud av",
+            "mejs.volume-slider": "Volymkontroll",
 
             // core/player.js
-            "mejs.video-player": "Video Player",
-            "mejs.audio-player": "Audio Player",
+            "mejs.video-player": "Video Spelare",
+            "mejs.audio-player": "Audio Spelare",
 
             // features/ads.js
-            "mejs.ad-skip": "Skip ad",
-            "mejs.ad-skip-info": ["Skip in 1 second", "Skip in %1 seconds"],
+            "mejs.ad-skip": "Hoppa över reklam",
+            "mejs.ad-skip-info": ["Hoppa in 1 second", "Hoppa in %1 sekunder"],
 
             // features/sourcechooser.js
-            "mejs.source-chooser": "Source Chooser",
+            "mejs.source-chooser": "Välj källa",
 
             // features/stop.js
-            "mejs.stop": "Stop",
+            "mejs.stop": "Stoppa",
 
             //features/speed.js
-            "mejs.speed-rate": "Speed Rate",
+            "mejs.speed-rate": "Hastighet",
 
             //features/progress.js
-            "mejs.live-broadcast": "Live Broadcast",
+            "mejs.live-broadcast": "Livesändning",
 
             // features/tracks.js
             "mejs.afrikaans": "Afrikaans",
-            "mejs.albanian": "Albanian",
-            "mejs.arabic": "Arabic",
-            "mejs.belarusian": "Belarusian",
-            "mejs.bulgarian": "Bulgarian",
-            "mejs.catalan": "Catalan",
-            "mejs.chinese": "Chinese",
-            "mejs.chinese-simplified": "Chinese (Simplified)",
-            "mejs.chinese-traditional": "Chinese (Traditional)",
-            "mejs.croatian": "Croatian",
-            "mejs.czech": "Czech",
-            "mejs.danish": "Danish",
-            "mejs.dutch": "Dutch",
-            "mejs.english": "English",
-            "mejs.estonian": "Estonian",
-            "mejs.filipino": "Filipino",
-            "mejs.finnish": "Finnish",
-            "mejs.french": "French",
-            "mejs.galician": "Galician",
-            "mejs.german": "German",
-            "mejs.greek": "Greek",
-            "mejs.haitian-creole": "Haitian Creole",
-            "mejs.hebrew": "Hebrew",
+            "mejs.albanian": "Albanska",
+            "mejs.arabic": "Arabiska",
+            "mejs.belarusian": "Nederländska",
+            "mejs.bulgarian": "Bulgariska",
+            "mejs.catalan": "Katalanska",
+            "mejs.chinese": "Kinesiska",
+            "mejs.chinese-simplified": "Kinesiska (Förenklad)",
+            "mejs.chinese-traditional": "Kinesiska (Traditionell)",
+            "mejs.croatian": "Kroatiska",
+            "mejs.czech": "Tjeckiska",
+            "mejs.danish": "Danska",
+            "mejs.dutch": "Holländska",
+            "mejs.english": "Engelska",
+            "mejs.estonian": "Estniska",
+            "mejs.filipino": "Filipinska",
+            "mejs.finnish": "Finska",
+            "mejs.french": "Franska",
+            "mejs.galician": "Galiciska",
+            "mejs.german": "Tyska",
+            "mejs.greek": "Grekiska",
+            "mejs.haitian-creole": "Haitisk kreolsk",
+            "mejs.hebrew": "Hebreiska",
             "mejs.hindi": "Hindi",
-            "mejs.hungarian": "Hungarian",
-            "mejs.icelandic": "Icelandic",
-            "mejs.indonesian": "Indonesian",
-            "mejs.irish": "Irish",
-            "mejs.italian": "Italian",
-            "mejs.japanese": "Japanese",
-            "mejs.korean": "Korean",
-            "mejs.latvian": "Latvian",
-            "mejs.lithuanian": "Lithuanian",
-            "mejs.macedonian": "Macedonian",
-            "mejs.malay": "Malay",
-            "mejs.maltese": "Maltese",
-            "mejs.norwegian": "Norwegian",
-            "mejs.persian": "Persian",
-            "mejs.polish": "Polish",
-            "mejs.portuguese": "Portuguese",
-            "mejs.romanian": "Romanian",
-            "mejs.russian": "Russian",
-            "mejs.serbian": "Serbian",
-            "mejs.slovak": "Slovak",
-            "mejs.slovenian": "Slovenian",
-            "mejs.spanish": "Spanish",
-            "mejs.swahili": "Swahili",
-            "mejs.swedish": "Swedish",
-            "mejs.tagalog": "Tagalog",
-            "mejs.thai": "Thai",
-            "mejs.turkish": "Turkish",
-            "mejs.ukrainian": "Ukrainian",
-            "mejs.vietnamese": "Vietnamese",
-            "mejs.welsh": "Welsh",
-            "mejs.yiddish": "Yiddish"
+            "mejs.hungarian": "Ungerska",
+            "mejs.icelandic": "Isländska",
+            "mejs.indonesian": "Indonesiska",
+            "mejs.irish": "Irländska",
+            "mejs.italian": "Italienska",
+            "mejs.japanese": "Japanska",
+            "mejs.korean": "Koreanska",
+            "mejs.latvian": "Lettiska",
+            "mejs.lithuanian": "Litauiska",
+            "mejs.macedonian": "Makedonska",
+            "mejs.malay": "Malaysiska",
+            "mejs.maltese": "Maltesiska",
+            "mejs.norwegian": "Norska",
+            "mejs.persian": "Persiska",
+            "mejs.polish": "Polska",
+            "mejs.portuguese": "Portugisiska",
+            "mejs.romanian": "Romänska",
+            "mejs.russian": "Ryska",
+            "mejs.serbian": "Serbiska",
+            "mejs.slovak": "Slovakiska",
+            "mejs.slovenian": "Slovenska",
+            "mejs.spanish": "Spanska",
+            "mejs.swahili": "Swahiliska",
+            "mejs.swedish": "Svenska",
+            "mejs.tagalog": "Tagalogiska",
+            "mejs.thai": "Thailänska",
+            "mejs.turkish": "Turkiska",
+            "mejs.ukrainian": "Ukrainska",
+            "mejs.vietnamese": "Vietnamesiska",
+            "mejs.welsh": "Skotska",
+            "mejs.yiddish": "Jiddisch"
         };
     }
 })(mejs.i18n);
