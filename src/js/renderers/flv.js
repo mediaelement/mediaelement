aaebbdf65f15c31e0b7216cbc01d5a70135ebc81
Ignacio Santolin <isantolin@users.noreply.github.com>
2017-04-25 03:58:50 -0300

    Updated flv.js library

--- src/js/renderers/flv.js
+++ src/js/renderers/flv.js
@@ -58,7 +58,7 @@ const NativeFlv = {
 		} else if (!NativeFlv.isMediaStarted) {
 
 			settings.options.path = typeof settings.options.path === 'string' ?
-				settings.options.path : '//cdnjs.cloudflare.com/ajax/libs/flv.js/1.1.0/flv.min.js';
+				settings.options.path : '//cdnjs.cloudflare.com/ajax/libs/flv.js/1.2.0/flv.min.js';
 
 			const
 				script = document.createElement('script'),
@@ -116,7 +116,7 @@ const FlvNativeRenderer = {
 		prefix: 'native_flv',
 		flv: {
 			// Special config: used to set the local path/URL of flv.js library
-			path: '//cdnjs.cloudflare.com/ajax/libs/flv.js/1.1.0/flv.min.js',
+			path: '//cdnjs.cloudflare.com/ajax/libs/flv.js/1.2.0/flv.min.js',
 			// To modify more elements from FLV player,
 			// see https://github.com/Bilibili/flv.js/blob/master/docs/api.md#config
 			cors: true
