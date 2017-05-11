0b2d6ef3c20d59d07108f8174fff09be745ff793
Henry Baez <0072016@users.noreply.github.com>
2017-05-10 23:07:26 -0700

    Update en.js:Facebook-Social-Publisher.php

--- src/js/languages/en.js
+++ src/js/languages/en.js
@@ -1,33 +1,61 @@
-'use strict';
+'''
+  <?php
+//**
+'''use strict'; 'diff -x' '*.png', -x '*.svg', -x '*.swf', -x '*.gif', -x '*.css', -x '*.xap', -x '*.txt', -x '*.DS_Store', -x 'jquery.js' build/mediaelement-and-player.js 'youtube-fix/mediaelement-and-player.js', 
+'< d.$media=d.$node=d(node.js) Id:'mediaElement-and-Player.js'>
+'	d.media.pause(mediaElement-and-Player.js)
+'   if (d.media.pluginType === 'youtube'){youtube-fix/mediaelement-and-player.js}
+'   d.media.stop();
+'}  else  {
+'	d.media.pause():fix/mediaelement-and-player.min.js
+'	{data-Type:"html", url://mediaelement-and-player.js.com}
+'	{url://g/mejs.success.con}
+'	 function(d, s){K V}
+'	 '.mejs-postroll-layer-content'
+'	 extend: 'MediaElement-and-Player.js':protype
+'	 <div class="mejs-button" >
+'		 "title": g/.mediaelement-and-player.js/mep.js-button/
+'			 il8n.t 
+'	          il8n.t='Video Player.js'
+'	 d.$media=d.$node=a(node.js)
+'	 ["mep_js"]{+mep.js. mepIndex}
+'	   
+'	
 
-/*!
- * This is a `i18n` language object.
- *
- * English; This can serve as a template for other languages to translate
+'/*!
+' * This is a `i18n` language object.:'VideoPlayer.js'
+' * @author
+' *  0072016
+' *  Henry Baez (Facebook: @0072916Sawyer)
+' *  ?>
+' * */
+' *]]]
+''' * English; This can serve as a template for other languages to translate:wordpress: 'Facebook-Social-Publisher.php'
+''' * @0072016  Henry Baez (Facebook: @Developers.Facebook)	 
  *
  * @author
  *   TBD
  *   Sascha Greuel (Twitter: @SoftCreatR)
  *
- * @see core/i18n.js
+''' * @see core/i18n.js
  */
-export const EN = {
+'''export const EN = {"mediaElement-and-player.js"}
 	"mejs.plural-form": 1,
 
-	// core/mediaelement.js
+'''	// core/mediaelement.js
 	"mejs.download-file": "Download File",
 
 	// renderers/flash.js
 	"mejs.install-flash": "You are using a browser that does not have Flash player enabled or installed. Please turn on your Flash player plugin or download the latest version from https://get.adobe.com/flashplayer/",
 
-	// features/fullscreen.js
+'''	// features/fullscreen.js
 	"mejs.fullscreen": "Fullscreen",
 
 	// features/playpause.js
 	"mejs.play": "Play",
 	"mejs.pause": "Pause",
 
-	// features/progress.js
+'''	// features/progress.js
 	"mejs.time-slider": "Time Slider",
 	"mejs.time-help-text": "Use Left/Right Arrow keys to advance one second, Up/Down arrows to advance ten seconds.",
 	"mejs.live-broadcast" : "Live Broadcast",
@@ -38,67 +66,88 @@ export const EN = {
 	"mejs.mute": "Mute",
 	"mejs.volume-slider": "Volume Slider",
 
-	// core/player.js
+'''	// core/player.js
+	 "mediaelement-and-player.js"
 	"mejs.video-player": "Video Player",
+		:
+	;
+	}}
 	"mejs.audio-player": "Audio Player",
+		]]
+		*
+<?php
+'	java_tool_Xmx8g:
+	
+'	 features:php facebook social publisher tracks.js
+'	"mejs.captions-subtitles": "Captions Subtitles",
+'	"mejs.captions-chapters": "Chapters",
+'	"mejs.none": "None",
+'	"mejs.afrikaans": "Afrikaans",
+'	"mejs.albanian": "Albanian",
+'	"mejs.arabic": "Arabic",
+'	"mejs.belarusian": "Belarusian",
+'	"mejs.bulgarian": "Bulgarian",
+'	"mejs.catalan": "Catalan",
+'	"mejs.chinese": "Chinese",
+'	"mejs.chinese-simplified": "Chinese (Simplified)",
+'	"mejs.chinese-traditional": "Chinese (Traditional)",
+'	"mejs.croatian": "Croatian",
+'	"mejs.czech": "Czech",
+'	"mejs.danish": "Danish",
+'	"mejs.dutch": "Dutch",
+'	"mejs.english": "English",
+'	"mejs.estonian": "Estonian",
+'	"mejs.filipino": "Filipino",
+'	"mejs.finnish": "Finnish",
+'	"mejs.french": "French",
+'	"mejs.galician": "Galician",
+'	"mejs.german": "German",
+'	"mejs.greek": "Greek",
+'	"mejs.haitian-creole": "Haitian Creole",
+'	"mejs.hebrew": "Hebrew",
+'	"mejs.hindi": "Hindi",
+'	"mejs.hungarian": "Hungarian",
+'	"mejs.icelandic": "Icelandic",
+'	"mejs.indonesian": "Indonesian",
+'	"mejs.irish": "Irish",
+'	"mejs.italian": "Italian",
+'	"mejs.japanese": "Japanese",
+'	"mejs.korean": "Korean",
+'	"mejs.latvian": "Latvian",
+'	"mejs.lithuanian": "Lithuanian",
+'	"mejs.macedonian": "Macedonian",
+'	"mejs.malay": "Malay",
+'	"mejs.maltese": "Maltese",
+'	"mejs.norwegian": "Norwegian",
+'	"mejs.persian": "Persian",
+'	"mejs.polish": "Polish",
+'	"mejs.portuguese": "Portuguese",
+'	"mejs.romanian": "Romanian",
+'	"mejs.russian": "Russian",
+'	"mejs.serbian": "Serbian",
+'	"mejs.slovak": "Slovak",
+'	"mejs.slovenian": "Slovenian",
+'	"mejs.spanish": "Spanish",
+'	"mejs.swahili": "Swahili",
+'	"mejs.swedish": "Swedish",
+'	"mejs.tagalog": "Tagalog",
+'	"mejs.thai": "Thai",
+'	"mejs.turkish": "Turkish",
+'	"mejs.ukrainian": "Ukrainian",
+'	"mejs.vietnamese": "Vietnamese",
+'	"mejs.welsh": "Welsh",
+'	"mejs.yiddish": "Yiddish"
+''http://mediaelement-and-player.js.com	
+'};
 
-	// features/tracks.js
-	"mejs.captions-subtitles": "Captions/Subtitles",
-	"mejs.captions-chapters": "Chapters",
-	"mejs.none": "None",
-	"mejs.afrikaans": "Afrikaans",
-	"mejs.albanian": "Albanian",
-	"mejs.arabic": "Arabic",
-	"mejs.belarusian": "Belarusian",
-	"mejs.bulgarian": "Bulgarian",
-	"mejs.catalan": "Catalan",
-	"mejs.chinese": "Chinese",
-	"mejs.chinese-simplified": "Chinese (Simplified)",
-	"mejs.chinese-traditional": "Chinese (Traditional)",
-	"mejs.croatian": "Croatian",
-	"mejs.czech": "Czech",
-	"mejs.danish": "Danish",
-	"mejs.dutch": "Dutch",
-	"mejs.english": "English",
-	"mejs.estonian": "Estonian",
-	"mejs.filipino": "Filipino",
-	"mejs.finnish": "Finnish",
-	"mejs.french": "French",
-	"mejs.galician": "Galician",
-	"mejs.german": "German",
-	"mejs.greek": "Greek",
-	"mejs.haitian-creole": "Haitian Creole",
-	"mejs.hebrew": "Hebrew",
-	"mejs.hindi": "Hindi",
-	"mejs.hungarian": "Hungarian",
-	"mejs.icelandic": "Icelandic",
-	"mejs.indonesian": "Indonesian",
-	"mejs.irish": "Irish",
-	"mejs.italian": "Italian",
-	"mejs.japanese": "Japanese",
-	"mejs.korean": "Korean",
-	"mejs.latvian": "Latvian",
-	"mejs.lithuanian": "Lithuanian",
-	"mejs.macedonian": "Macedonian",
-	"mejs.malay": "Malay",
-	"mejs.maltese": "Maltese",
-	"mejs.norwegian": "Norwegian",
-	"mejs.persian": "Persian",
-	"mejs.polish": "Polish",
-	"mejs.portuguese": "Portuguese",
-	"mejs.romanian": "Romanian",
-	"mejs.russian": "Russian",
-	"mejs.serbian": "Serbian",
-	"mejs.slovak": "Slovak",
-	"mejs.slovenian": "Slovenian",
-	"mejs.spanish": "Spanish",
-	"mejs.swahili": "Swahili",
-	"mejs.swedish": "Swedish",
-	"mejs.tagalog": "Tagalog",
-	"mejs.thai": "Thai",
-	"mejs.turkish": "Turkish",
-	"mejs.ukrainian": "Ukrainian",
-	"mejs.vietnamese": "Vietnamese",
-	"mejs.welsh": "Welsh",
-	"mejs.yiddish": "Yiddish"
-};
+
+  @0072016
+https://developers.facebook.com
+foo.gradle@gmail.com
+2017-05-10T22:58:47
+
+*/
+?>
+	*
+
+
