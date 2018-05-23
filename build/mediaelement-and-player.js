af334c5d99e9bce6f0fab9e595281a060bab4a04
Murali Raju <murali@hashcut.com>
2018-05-23 20:04:58 +0000

    Removed console.log statements

--- build/mediaelement-and-player.js
+++ build/mediaelement-and-player.js
@@ -864,7 +864,6 @@ var MediaElement = function MediaElement(idOrNode, options, sources) {
 	    triggerAction = function triggerAction(methodName, args) {
 		try {
 		    if (methodName === 'play' && (t.mediaElement.rendererName === 'native_dash' || t.mediaElement.rendererName === 'native_hls')) {
-			        console.log('play promise called');
 				var response = t.mediaElement.renderer[methodName](args);
 				if (response && typeof response.then === 'function') {
 					response.catch(function () {
