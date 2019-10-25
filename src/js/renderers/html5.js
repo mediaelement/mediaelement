b80cba60fe2c616cdf3f54709f53b6ffb2f6b315
Daniel Jagszent <daniel@jagszent.de>
2019-10-25 16:39:00 +0200

    Make error event handler more robust
    Check if event.target.error exists before accessing it.
    In some edge cases e.target is no <video> element or it has null as error property.

--- src/js/renderers/html5.js
+++ src/js/renderers/html5.js
@@ -138,7 +138,7 @@ const HtmlMediaElement = {
 		// Check if it current source can be played; otherwise, load next until no more options are left
 		node.addEventListener('error', function (e) {
 			// Reload the source only in case of the renderer is active at the moment
-			if (e.target.error.code === 4 && isActive) {
+			if (e && e.target && e.target.error && e.target.error.code === 4 && isActive) {
 				if (index < total && mediaFiles[(index + 1)] !== undefined) {
 					node.src = mediaFiles[index++].src;
 					node.load();
