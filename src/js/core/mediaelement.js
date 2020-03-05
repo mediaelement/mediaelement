40bbf18b4b32a44131ec7a851e62c70670ef536c
ihor Paliychuk <mansonigor@gmail.com>
2020-03-05 14:58:49 +0200

    add separator

--- src/js/core/mediaelement.js
+++ src/js/core/mediaelement.js
@@ -385,7 +385,7 @@ class MediaElement {
 				}
 
 				// turn on the renderer (this checks for the existing renderer already)
-				var shouldChangeRenderer = !(mediaFiles[0].src === undefined || mediaFiles[0].src === null || mediaFiles[0].src === '')
+				var shouldChangeRenderer = !(mediaFiles[0].src === undefined || mediaFiles[0].src === null || mediaFiles[0].src === '');
 				return shouldChangeRenderer ? t.mediaElement.changeRenderer(renderInfo.rendererName, mediaFiles) : null;
 			},
 			triggerAction = (methodName, args) => {
